444f7199ad5491bbd996f2436a2520c8918d4ad2
Marcin Osowski <marcin@osowski.info>
2019-05-17 20:35:04 +0200

    Separate directories for logger and frontend

---
+++ frontend/config.py
@@ -0,0 +1,11 @@
+# Database settings.
+GCP_CREDENTIALS="./gcp-credentials.json"
+GCP_PROJECT="pogoda-240600"
+GCP_KIND_PREFIX="reading:"
+
+# Web server settings.
+PROD_HTTP_HOST='127.0.0.1'
+PROD_HTTP_PORT=8092
+
+DEV_HTTP_HOST='127.0.0.1'
+DEV_HTTP_PORT=8080
--- logger/config.py
+++ logger/config.py
@@ -1,4 +1,3 @@
-# Logger settings.
 COMM_PORT="/dev/ttyUSB0"
 LOGGER_INTERVAL_SEC=120
 NAME_TRANSLATION={
@@ -7,17 +6,11 @@ NAME_TRANSLATION={
     "Water level": "water_level",
     "Pressure": "pressure",
 }
+
 # Limits RAM usage in case of DB unreachability.
 MAX_QUEUE_SIZE=1024*1024
 
-# Common settings.
+# Database settings.
 GCP_CREDENTIALS="./gcp-credentials.json"
 GCP_PROJECT="pogoda-240600"
 GCP_KIND_PREFIX="reading:"
-
-# Web server settings.
-PROD_HTTP_HOST='127.0.0.1'
-PROD_HTTP_PORT=8092
-
-DEV_HTTP_HOST='127.0.0.1'
-DEV_HTTP_PORT=8080
