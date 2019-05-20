4c11f7e83891b45235a30c3f68704570417feb97
Marcin Osowski <marcin@osowski.info>
2019-05-20 22:06:34 +0200

    Log air quality data

--- logger/config.py
+++ logger/config.py
@@ -5,6 +5,20 @@ NAME_TRANSLATION={
     "Temperature": "temperature",
     "Water level": "water_level",
     "Pressure": "pressure",
+
+    "PM 1.0 standard": "pm_10_std",
+    "PM 2.5 standard": "pm_25_std",
+    "PM 10.0 standard": "pm_100_std",
+    "PM 1.0 environmental": "pm_10_env",
+    "PM 2.5 environmental": "pm_25_env",
+    "PM 10.0 environmental": "pm_100_env",
+
+    "Particles > 0.3um / 0.1L air": "particles_03",
+    "Particles > 0.5um / 0.1L air": "particles_05",
+    "Particles > 1.0um / 0.1L air": "particles_10",
+    "Particles > 2.5um / 0.1L air": "particles_25",
+    "Particles > 5.0um / 0.1L air": "particles_50",
+    "Particles > 10.0 um / 0.1L air": "particles_100",
 }
 
 # Limits RAM usage in case of DB unreachability.
