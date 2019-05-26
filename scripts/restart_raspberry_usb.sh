6f7a7ab7df94a28719809602cd2f10f69e3e8305
Marcin Osowski <marcin@osowski.info>
2019-05-26 22:18:39 +0200

    Added a script to restart Raspberry USB (reloads the Arduino)

---
+++ scripts/restart_raspberry_usb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Disabling USB
+echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
+cat /sys/devices/platform/soc/3f980000.usb/buspower
+
+sleep 5
+
+echo Enabling USB
+echo 1 > /sys/devices/platform/soc/3f980000.usb/buspower
+cat /sys/devices/platform/soc/3f980000.usb/buspower
+
+echo Done.
