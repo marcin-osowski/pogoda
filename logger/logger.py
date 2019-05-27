d8ef408fbd0588414aeb2984eb68675d8ddbe80b
Marcin Osowski <marcin@osowski.info>
2019-05-27 17:55:28 +0200

    Renamed queue to readings queue. Removed queue monitor.

--- logger/logger.py
+++ logger/logger.py
@@ -13,14 +13,19 @@ import instance_config
 import data_source
 
 
+# Readings queue - will be filled with data
+# read from the sensors, to be written to the DB.
+readings_queue = queue.Queue()
+
+
 def create_datastore_client():
     """Creates a Client, to connect to the Datastore DB."""
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.GCP_CREDENTIALS
     return datastore.Client(project=config.GCP_PROJECT)
 
 
-def get_and_push_queue(weather_data, data_queue):
-    """Retrieves data and inserts it into a queue, once."""
+def get_and_push_readings_queue(weather_data):
+    """Retrieves data and inserts it into the readings queue, once."""
     for comm_name, name in config.NAME_TRANSLATION.items():
         value, timestamp = weather_data.readings[comm_name].get_with_timestamp()
         if value is None:
@@ -33,23 +38,23 @@ def get_and_push_queue(weather_data, data_queue):
         if latency >= datetime.timedelta(seconds=config.LOGGER_INTERVAL_SEC):
             # Data too old
             continue
-        data_queue.put((name, value, timestamp))
+        readings_queue.put((name, value, timestamp))
 
 
-def queue_producer_loop(data_queue):
+def readings_queue_producer_loop():
     # Uses a backgroud thread to read values from the serial port.
     weather_data = data_source.WeatherDataSource
 
     while True:
         try:
             time.sleep(config.LOGGER_INTERVAL_SEC)
-            if data_queue.qsize() >= config.MAX_QUEUE_SIZE:
+            if readings_queue.qsize() >= config.MAX_QUEUE_SIZE:
                 # Dropping data, queue too long.
                 pass
             else:
-                get_and_push_queue(weather_data, data_queue)
+                get_and_push_readings_queue(weather_data, readings_queue)
         except:
-            print("Problem while inserting data to the queue.")
+            print("Problem while inserting data to the readings queue.")
             traceback.print_exc()
             time.sleep(60.0)
 
@@ -68,64 +73,34 @@ def insert_into_db(client, name, value, timestamp):
     client.put(reading_ent)
 
 
-def queue_consumer_loop(data_queue):
-    """A loop for popping items from the queue and inserting them into the DB."""
+def readings_queue_consumer_loop():
+    """A loop for popping items from the readings queue and inserting them into the DB."""
     while True:
         try:
             client = create_datastore_client()
             while True:
-                name, value, timestamp = data_queue.get()
+                name, value, timestamp = readings_queue.get()
                 written = False
                 try:
                     insert_into_db(client, name, value, timestamp)
                     written = True
                 finally:
                     if not written:
-                        # Put back in the queue
-                        data_queue.put((name, value, timestamp))
+                        # Put back in the readings queue
+                        readings_queue.put((name, value, timestamp))
         except:
             print("Problem while inserting data into the DB.")
             traceback.print_exc()
             time.sleep(120.0)
 
 
-def queue_monitor_loop(data_queue):
-    """A thread to monitor the state of the queue
-
-    Checks every 45 minutes how many elements are pending.
-    If there's more than 25 elements pending then prints
-    a warning message.
-    """
-    while True:
-        time.sleep(45.0 * 60.0)
-        size = data_queue.qsize()
-        if size > 25:
-            print("%s: Warning: %d elements pending in the DB queue" % (
-                    datetime.datetime.now().isoformat(" "),
-                    size))
-
-
 if __name__ == "__main__":
-    # Queue - will be filled with data to be written to the DB.
-    data_queue = queue.Queue()
-
-    # Start a background thread to push values to the queue.
-    producer_thread = threading.Thread(
-            target=queue_producer_loop,
-            kwargs=dict(data_queue=data_queue),
-    )
+    # Start a background thread to push values to the readings queue.
+    producer_thread = threading.Thread(target=readings_queue_producer_loop)
     producer_thread.setDaemon(True)
     producer_thread.start()
 
-    # Start a background thread to monitor the queue.
-    monitor_thread = threading.Thread(
-            target=queue_monitor_loop,
-            kwargs=dict(data_queue=data_queue),
-    )
-    monitor_thread.setDaemon(True)
-    monitor_thread.start()
 
-
-    # Start popping items from the queue and inserting them into the DB.
-    queue_consumer_loop(data_queue)
+    # Start popping items from the readings queue and inserting them into the DB.
+    readings_queue_consumer_loop()
 
