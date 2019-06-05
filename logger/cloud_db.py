eb976982e8a6fb06aba6f1ef3465383390fe5ff4
Marcin Osowski <marcin@osowski.info>
2019-06-05 14:04:46 +0200

    Moved cloud DB code to a separate file

---
+++ logger/cloud_db.py
@@ -0,0 +1,43 @@
+from google.cloud import datastore
+import os
+import time
+
+import config
+
+
+def create_datastore_client():
+    """Creates a Client, to connect to the Datastore DB."""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.GCP_CREDENTIALS
+    return datastore.Client(project=config.GCP_PROJECT)
+
+
+def insert_into_cloud_db(client, timestamp, kind, value):
+    """Inserts a single entry into the cloud DB."""
+    key = client.key(kind)
+    reading_ent = datastore.Entity(key)
+    reading_ent.update(dict(timestamp=timestamp))
+    if value is not None:
+        reading_ent.update(dict(value=value))
+    client.put(reading_ent)
+
+
+def cloud_uploader_loop(data_queue):
+    """A loop: popping items from queue, inserting them into the cloud DB."""
+    while True:
+        try:
+            client = create_datastore_client()
+            while True:
+                timestamp, kind, value = data_queue.get_youngest()
+                written = False
+                try:
+                    insert_into_cloud_db(client, timestamp, kind, value)
+                    written = True
+                finally:
+                    if not written:
+                        # Put back in the readings queue
+                        data_queue.put(timestamp, kind, value)
+        except Exception as e:
+            print("Problem while inserting data into the cloud DB.")
+            print(e)
+            time.sleep(120.0)
+
--- logger/logger.py
+++ logger/logger.py
@@ -1,53 +1,12 @@
 #!/usr/bin/env python3
 
-import datetime
-from google.cloud import datastore
-import os
 import threading
 import time
 
 import arduino_interface
-import config
+import cloud_db
 import custom_queue
-import instance_config
 import ping
-
-
-def create_datastore_client():
-    """Creates a Client, to connect to the Datastore DB."""
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.GCP_CREDENTIALS
-    return datastore.Client(project=config.GCP_PROJECT)
-
-
-def insert_into_cloud_db(client, timestamp, kind, value):
-    """Inserts a single entry into the cloud DB."""
-    key = client.key(kind)
-    reading_ent = datastore.Entity(key)
-    reading_ent.update(dict(timestamp=timestamp))
-    if value is not None:
-        reading_ent.update(dict(value=value))
-    client.put(reading_ent)
-
-
-def cloud_uploader_loop(data_queue):
-    """A loop: popping items from queue, inserting them into the cloud DB."""
-    while True:
-        try:
-            client = create_datastore_client()
-            while True:
-                timestamp, kind, value = data_queue.get_youngest()
-                written = False
-                try:
-                    insert_into_cloud_db(client, timestamp, kind, value)
-                    written = True
-                finally:
-                    if not written:
-                        # Put back in the readings queue
-                        data_queue.put(timestamp, kind, value)
-        except Exception as e:
-            print("Problem while inserting data into the cloud DB.")
-            print(e)
-            time.sleep(120.0)
 
 
 if __name__ == "__main__":
@@ -72,7 +31,7 @@ if __name__ == "__main__":
 
     # Start popping items from the readings queue and inserting them into the DB.
     cloud_uploader_thread = threading.Thread(
-        target=cloud_uploader_loop,
+        target=cloud_db.cloud_uploader_loop,
         kwargs=dict(data_queue=data_queue)
     )
     cloud_uploader_thread.setDaemon(True)
